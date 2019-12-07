963618d20b8cf89517173494b802f58b6b9ad9ef
chenxd1 <1029776889@qq.com>
2019-12-07 20:43:43 +0800

    逢7过“
    git config --global user.email 1029776889@qq.com

---
+++ jump7.py
@@ -0,0 +1,12 @@
+for i in range (1,101):
+    if i % 7 ==0 or i % 10== 7 or i // 10 == 7:
+    continue
+    print(i)
+
+a=0
+while a < 100:
+    if a % 7 ==0 or a % 10== 7 or a // 10 == 7:
+        continue
+    a=a+1
+    print(a)
+
