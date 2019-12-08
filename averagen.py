39844499b9ccb483bc2c0975dc36454a1ad7dd71
chenxd1 <1029776889@qq.com>
2019-12-08 14:00:38 +0800

    录入十个数算平均数&华氏度转为摄氏度

---
+++ averagen.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+#计算10个数的平均值
+N=10
+sum=0
+count=0
+print('请输入10个数字: ')
+while count < N:
+    number = float(input())
+    sum=sum + number
+    count = count + 1
+average = sum / 10
+print('N = {},Sum = {}'.format(N,sum))
+print('average={:.2f}'.format(average))
+
