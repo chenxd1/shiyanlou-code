7d93ff88501c0a3876809c0ce30c87840d36da37
chenxd1 <1029776889@qq.com>
2019-12-09 13:33:05 +0800

    循环

---
+++ multiplication.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+print('-' * 50)
+i = 1
+while i < 10:
+    n = 1
+    while n < 10:
+        print("{:5d}".format(i * n), end =' ')
+        n +=1
+    print()
+    i +=1
+print('-' * 50)
+ 
